8230751924985b82ca7278efd15fe347292a64d5
Huynh Nhan Ngo <ngohuynhnhan@gmail.com>
2023-02-16 22:15:41 -0800

    Add voice options populate

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,43 @@ import React from "react";
 import "./App.css";
 
 function App() {
+  const [input, setInput] = React.useState("");
+  const [voices, setVoices] = React.useState<SpeechSynthesisVoice[]>([]);
+
+  React.useEffect(() => {
+    // Loading time for website to load the voices
+    let id = setInterval(() => {
+      if (window.speechSynthesis.getVoices().length !== 0) {
+        clearInterval(id);
+        setVoices(window.speechSynthesis.getVoices());
+      }
+    }, 100);
+  }, []);
+
+  const onClick = () => {
+    const utterThis = new SpeechSynthesisUtterance(input);
+    window.speechSynthesis.speak(utterThis);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <textarea className="textarea"></textarea>
-        <button className="button">Speak</button>
+        <textarea
+          className="textarea"
+          onChange={(e) => setInput(e.target.value)}
+        ></textarea>
+        <div>
+          <select className="voiceOptions">
+            {voices.map((v, idx) => (
+              <option key={idx} value={idx}>
+                {v.name}
+              </option>
+            ))}
+          </select>
+          <button className="button" onClick={onClick}>
+            Speak
+          </button>
+        </div>
         <div className="output"></div>
       </header>
     </div>
